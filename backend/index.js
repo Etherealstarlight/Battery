30d3d17eda8d23a09bbfbbaabfcfe697523a73a0
Donnicool <the_first_star@mail.ru>
2024-04-07 04:16:56 +0700

    Bettery-9 Vercel

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
-// const sequelize = require('./db')
-// const models = require('./models/models')
+const sequelize = require('./db')
+const models = require('./models/models')
 const cors = require('cors')
 const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
@@ -15,36 +15,36 @@ app.use('/api', router)
 
 app.use(errorHandler)
 
-// const start = async () => {
-//   try {
-//     await sequelize.authenticate()
-//     await sequelize.sync()
-//     app.listen(PORT, () => {
-//       console.log(`Server Connection Successful on ${PORT}`)
-//     })
-//   } catch (e) {
-//     console.log(e)
-//   }
-// }
+const start = async () => {
+  try {
+    app.listen(PORT, () => {
+      console.log(`Server Connection Successful on ${PORT}`)
+    })
+    await sequelize.authenticate()
+    await sequelize.sync()
+  } catch (e) {
+    console.log(e)
+  }
+}
 
-// start()
+start()
 
 console.log('STARTED')
 
-// setInterval(async () => {
-//   const batteries = await models.Battery.findAll()
+setInterval(async () => {
+  const batteries = await models.Battery.findAll()
 
-//   batteries.forEach(async (battery) => {
-//     if (battery.percents > 1) {
-//       await models.Battery.update(
-//         { percents: Number(battery.percents) >= 11 ? Number(battery.percents) - 10 : 1 },
-//         {
-//           where: { id: battery.id },
-//         }
-//       )
-//     }
-//   })
-// }, 100000)
+  batteries.forEach(async (battery) => {
+    if (battery.percents > 1) {
+      await models.Battery.update(
+        { percents: Number(battery.percents) >= 11 ? Number(battery.percents) - 10 : 1 },
+        {
+          where: { id: battery.id },
+        }
+      )
+    }
+  })
+}, 100000)
 
 app.get('/', (request, response) => {
   response.status(200).json({ message: 'SUCCESS' })
--- backend/models/models.js
+++ backend/models/models.js
@@ -1,22 +1,22 @@
-// const sequelize = require('../db')
-// const { DataTypes } = require('sequelize')
+const sequelize = require('../db')
+const { DataTypes } = require('sequelize')
 
-// const User = sequelize.define('user', {
-//   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-//   email: { type: DataTypes.STRING, unique: true },
-//   password: { type: DataTypes.STRING },
-//   role: { type: DataTypes.STRING, defaultValue: 'USER' },
-// })
+const User = sequelize.define('user', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  email: { type: DataTypes.STRING, unique: true },
+  password: { type: DataTypes.STRING },
+  role: { type: DataTypes.STRING, defaultValue: 'USER' },
+})
 
-// const Battery = sequelize.define('battery', {
-//   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-//   percents: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
-// })
+const Battery = sequelize.define('battery', {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  percents: { type: DataTypes.INTEGER, allowNull: false, defaultValue: 0 },
+})
 
-// User.hasOne(Battery)
-// Battery.belongsTo(User)
+User.hasOne(Battery)
+Battery.belongsTo(User)
 
-// module.exports = {
-//   User,
-//   Battery,
-// }
+module.exports = {
+  User,
+  Battery,
+}
